e44f64090221c7c7c0860f1094998f0904373341
Adrian Tate <adrian@MacBookPro-AdrianT.local>
2024-03-02 06:44:38 +0000

    enables intermediate storage of all intermediate files for information processing improvement purposes, via PRODUCE_INTERMEDIATES

--- run_assistant_thread.py
+++ run_assistant_thread.py
@@ -1,6 +1,7 @@
 import os
 import time, re
 import json
+import sys
 from dochandler import Rdoc
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
@@ -32,7 +33,7 @@ def success_criteria_met(client,thread):
             return file_from_response
     return None
 
-def overseer_manage_assistant(client, assistant_function, *args, max_retries=3):
+def overseer_manage_assistant(client, write_intermediate, prefix, index, assistant_function, *args, max_retries=3):
     for attempt in range(max_retries):
         print(f"Attempt {attempt + 1} for {assistant_function.__name__}")
         run_steps, retrieve_response, thread = assistant_function(client, *args)
@@ -41,6 +42,11 @@ def overseer_manage_assistant(client, assistant_function, *args, max_retries=3):
         file = success_criteria_met(client,thread)
         if file:
             print(f"good file from retrieval on attempt {attempt + 1}")
+
+            if write_intermediate:
+                json_content = download_file_by_id(client, file)
+                with open(f"./Intermediates/insight_{prefix}_{index}.json", "w", encoding="utf-8") as json_file:
+                    json_file.write(json_content)
             return file
         # If not successful, reply to the thread and insist on the file
         print("Success criteria not met. Requesting refinement or a different approach.")
@@ -73,18 +79,16 @@ def retrieve_run(client, thread_id, run_id, max_retries, description):
     print(f"Run {run_id} did not complete after {max_retries} retries.")
     return None  # Return None if all retries fail
 def run_assistant(client, assistant, prompt, file_ids, description):
-    print(file_ids)
+    print(f" running assistant with files {file_ids}")
     thread = client.beta.threads.create(
         messages=[
             {
                 "role": "user",
                 "content": f"Read the files {file_ids} using code_interpreter. {prompt}",
                 "file_ids": file_ids
-
             }
         ]
     )
-
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
         assistant_id=assistant,
@@ -113,6 +117,10 @@ def retrieve_file_annotation(client, thread):
                     return file
     print("No annotations for a file were found")
     return None
+
+def download_file_by_id(client, file_id):
+    content = client.files.retrieve_content(file_id)
+    return content
 
 def return_json_and_file(client,thread):
     # Sometimes naughty little AIs still send via response, so must extract it via regular expressions
@@ -149,15 +157,15 @@ def return_json_and_file(client,thread):
     print("No JSON content was found")
     return None
 
-
-def parallel_file_process(client, file_dir):
+def parallel_file_process(client, file_dir, company_data, prefix, write_intermediates):
     files = [f for f in os.listdir(file_dir) if os.path.isfile(os.path.join(
         file_dir, f))]
     index = 0
     # # parallel loop - submit the process_file function on as many threads as there are files
     # TODO: change the number of files per worker (single file per worker currently)
     with ThreadPoolExecutor(max_workers=len(files)) as executor:
-        future_to_file = {executor.submit(process_file, client, file_name, index + i, file_dir): file_name
+        future_to_file = {executor.submit(process_file, client, file_name, index + i, file_dir, company_data, prefix,
+                                          write_intermediates): file_name
                           for i, file_name in enumerate(files)}
         for future in as_completed(future_to_file):
             file_name = future_to_file[future]
@@ -168,50 +176,72 @@ def parallel_file_process(client, file_dir):
                 print(f"Generated an exception: {exc}")
     return None
 
-#TODO mmove file handling into appropriate module
-def import_data_file(data_dir, file_name):
-    file_path = os.path.join(data_dir, file_name)  # Full path to the file
-    _, file_extension = os.path.splitext(file_name)  # Extract file extension
-    format = file_extension.lstrip('.')  # Remove the leading '.' from the extension
-    print(f"format is {format}")
-    doc = Rdoc.create(file_path, format, "data")
-    json_doc = doc.build_structured_data()
-    print(json_doc)
-    return json_doc
+# TODO move file handling into appropriate module
+def import_data_files(client, data_dir):
+    # import all the files in the given directory and optionally write intermediates
+    data_files = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
+    company_data = []  # company_data will be a list of json objects containing info about the company
+    for i, file_name in enumerate(data_files):
+        file_path = os.path.join(data_dir, file_name)  # Full path to the file
+        _, file_extension = os.path.splitext(file_name)  # Extract file extension
+        format = file_extension.lstrip('.')  # Remove the leading '.' from the extension
+        print(f"format is {format}")
+        doc = Rdoc.create(file_path, format, "data")
+        json_doc = doc.build_structured_data()
+        with open(f"./Intermediates/structured_data_{i}.json", "w", encoding="utf-8") as json_file:
+            json_file.write(json_doc)
+        with open(f"./Intermediates/structured_data_{i}.json", "rb") as json_openai_file:
+            json_openai_response = client.files.create(
+                file=json_openai_file,
+                purpose="assistants"
+            )
+        print(f"wrote file ./Intermediates/structured_data_{i}.json and uploaded to {json_openai_response.id}")
+    return json_openai_response
 
-def process_file(client, file_name, index, insight_dir):
+def process_file(client, file_name, index, insight_dir, company_data, prefix, write_intermediate):
 
     # step 1 : extract the filename and create dochandler class
 
     file_path = os.path.join(insight_dir, file_name)  # Full path to the file
     _, file_extension = os.path.splitext(file_name)  # Extract file extension
     format = file_extension.lstrip('.')  # Remove the leading '.' from the extension
+    print(f"format is for {file_name} is {format} and prefix is {prefix}")
     doc = Rdoc.create(file_path, format, "insight")
 
     # step 2 : convert to structured format (json)
 
     json_doc = doc.build_structured_data()
-    print(json_doc)
+    # print(json_doc)
 
-    # step 3 : condense to remove excess data
+    # step 3 : condense to remove excess data (needs a written file creation)
 
-    with open(f"structured_document_{index}.json", "w", encoding="utf-8") as json_file:
+    with open(f"./Intermediates/structured_{prefix}_{index}.json", "w", encoding="utf-8") as json_file:
         json_file.write(json_doc)
-    with open(f"structured_document_{index}.json", "rb") as json_openai_file:
+    with open(f"./Intermediates/structured_{prefix}_{index}.json", "rb") as json_openai_file:
         json_openai_response = client.files.create(
             file=json_openai_file,
             purpose="assistants"
         )
-    condense_file = overseer_manage_assistant(client, run_condense_analysis, json_openai_response)
-    # step 4 : extract insights
+
+    condense_file = overseer_manage_assistant(client, write_intermediate, prefix, index, run_condense_analysis,
+                                              json_openai_response)
+
     if condense_file:
-        insight_file = overseer_manage_assistant(client, run_insight_analysis, condense_file)
+        insight_file = overseer_manage_assistant(client, write_intermediate, prefix, index, run_insight_analysis,
+                                                 condense_file, company_data)
         return insight_file
     else:
         return None
 
-def run_insight_analysis(client, condense_file):
-    insight_prompt = f"Condense the json file {condense_file}"
+def download_content_and_write(client,agent_file,local_filename):
+    json_content = download_file_by_id(client, agent_file)
+    with open(f"./Intermediates/{local_filename}.json", "w", encoding="utf-8") as json_file:
+        json_file.write(json_content)
+    return json_file
+def run_insight_analysis(client, condense_file, data_file):
+    insight_prompt = (f"Generate the insights that relate to the company NAG Or Numerical Algorithms Group "
+                      f"with basic information contained in the file {data_file} and potential insights "
+                      f"in {condense_file}")
     insight_steps, insight_response, insight_thread = run_assistant(client, insight_agent, insight_prompt,
                                                                    file_ids=[condense_file],
                                                                    description=insight_description)
@@ -233,13 +263,15 @@ def run_trends_analysis(client, insight_files, data_file):
                                                                  description=trends_description)
     return trends_steps, trends_response, trends_thread  # Modified to return necessary info
 
+
 def run_capabilities_analysis(client, insight_files, data_file):
-    capabilities_prompt = (f"Generate the capabilities that possessed by NAG Or Numerical Algorithms Group with basic information "
+    capabilities_prompt = (f"Generate the capabilities possessed by NAG Or Numerical Algorithms Group with basic information "
                      f"contained in the file {data_file} and collected insights in {insight_files}")
     capabilities_steps, capabilities_response, capabilities_thread = run_assistant(client, capabilities_agent, capabilities_prompt,
                                                                  file_ids=insight_files,
                                                                  description=capabilities_description)
     return capabilities_steps, capabilities_response, capabilities_thread
+
 
 def run_challenges_analysis(client, trends_file, capabilities_file):
     challenges_prompt = (f"Generate the challenges that NAG Or Numerical Algorithms Group faces, using the files {trends_file} "
@@ -249,6 +281,7 @@ def run_challenges_analysis(client, trends_file, capabilities_file):
                                                                                    file_ids=[trends_file,capabilities_file],
                                                                                    description=challenges_description)
     return challenges_steps, challenges_response, challenges_thread
+
 
 def run_actions(client, challenge, trends_file, capabilities_file):
     actions_prompt = (f"Your consultant colleagues have decided that the top strategic challenge facing the company "
