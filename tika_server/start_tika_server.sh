a3a770593b74837cf9f1223eda8cbee1ea66b807
leo <leo.grill@nag.com>
2024-07-22 09:11:53 +0200

    Update tika server location

--- tika_server/start_tika_server.sh
+++ tika_server/start_tika_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export TIKA_SERVER_JAR = ${PWD}
-TIKA_SERVER_JAR = 'PATH_OF_FOLDER_CONTAINING_TIKA_SERVER_JAR'.
+export TIKA_SERVER_JAR=${PWD}/tika_server
+# TIKA_SERVER_JAR = 'PATH_OF_FOLDER_CONTAINING_TIKA_SERVER_JAR'.
 # java -jar tika-server-standard-2.9.2.jar --port 9999 &
